2511e5828f3f7521bf788557d3b11bfc0e28f400
everton_goncalves <everton.goncalves.89@gmail.com>
2024-10-08 13:53:15 -0300

    feat: reference vs value

--- fundamentals/pointers/pointers.go
+++ fundamentals/pointers/pointers.go
@@ -2,6 +2,10 @@ package pointers
 
 import "fmt"
 
+type Account struct {
+	balance float64
+}
+
 func PointerExample() {
 	fmt.Println("> pointerExample")
 	a := 10
@@ -27,4 +31,42 @@ func PointerExample() {
 		a: 90, *aPointer: 90, *b: 90
 		&a: 0xc00009a1d0, aPointer: 0xc00009a1d0, b: 0xc00009a1d0
 	*/
+
+	account := Account{
+		balance: 100.0,
+	}
+	fmt.Printf("addWithoutChange: %v\n", account.addWithoutChange(50))
+	fmt.Printf("account.total: %v\n", account.balance)
+
+	fmt.Printf("add: %v\n", account.add(50))
+	fmt.Printf("account.total: %v\n", account.balance)
+
+	newAccount := createAccount(20)
+	fmt.Printf("create account: %v\n", newAccount.balance)
+
+	/*
+		addWithoutChange: 150
+		account.total: 100
+		add: 150
+		account.total: 150
+		create account: 20
+	*/
+}
+
+func (a Account) addWithoutChange(value float64) float64 {
+	// we are working with a copy of the account
+	a.balance += value
+
+	return a.balance
+}
+
+func (a *Account) add(value float64) float64 {
+	// we are working with the account reference
+	a.balance += value
+
+	return a.balance
+}
+
+func createAccount(balance float64) *Account {
+	return &Account{balance}
 }
