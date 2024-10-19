3a4d87ce37f8903651b09ecb92cdc15ebcd5da54
everton_goncalves <everton.goncalves.89@gmail.com>
2024-10-19 13:43:28 -0300

    feat: slices
    - slices do not store data

--- fundamentals/slices/slice.go
+++ fundamentals/slices/slice.go
@@ -36,8 +36,24 @@ func SliceExample() {
 		anotherSlice -> length: 4, capacity: 6, value: [1 2 3 4]
 		predefinedSlice -> length: 0, capacity: 10, value: []
 	*/
+	changeValue()
 }
 
 func printSlice(name string, slice []int) {
 	fmt.Printf("%v -> length: %d, capacity: %d, value: %v\n", name, len(slice), cap(slice), slice)
+}
+
+// slice doesn't store data, it is just a reference to an array
+func changeValue() {
+	fmt.Println("> changeValue")
+	var colors = [4]string{"blue", "red", "yellow", "white"}
+	filter1 := colors[1:4]
+	fmt.Println("filter1:", filter1) // filter1: [red yellow white]
+	filter2 := colors[0:3]
+	fmt.Println("filter2:", filter2) // filter1: [blue red yellow]
+
+	filter2[2] = "gray"              // this change is made in the colors array and will be reflected in all slices
+	fmt.Println("filter1:", filter1) // filter1: [red gray white]
+	fmt.Println("filter2:", filter2) // filter1: [blue red gray]
+
 }
