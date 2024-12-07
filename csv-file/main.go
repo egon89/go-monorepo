a3160ef027cfb15ff772b7cfd57de014d76e7007
everton_goncalves <everton.goncalves.89@gmail.com>
2024-12-07 00:29:27 -0300

    feat: reading file
    - all file
    - line by line

--- csv-file/main.go
+++ csv-file/main.go
@@ -14,9 +14,14 @@ type User struct {
 }
 
 func main() {
-	fmt.Println("csv-file")
+	readAllFile()
+	readLineByLine()
+}
 
-	path := "./resource/users.csv"
+func readAllFile() {
+	fmt.Println("> reading all file")
+
+	path := "./resource/users_1k.csv"
 
 	file, err := os.Open(path)
 	if err != nil {
@@ -59,5 +64,50 @@ func main() {
 
 	for _, user := range users {
 		fmt.Printf("User: %+v\n", user) // %+v prints field:value
+	}
+}
+
+func readLineByLine() {
+	fmt.Println("> reading line by line")
+
+	path := "./resource/users_10k.csv"
+
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	header, err := reader.Read()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Headers:", header)
+
+	for {
+		row, err := reader.Read()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			fmt.Println("Error reading row:", err)
+			continue
+		}
+
+		if len(row) != 4 {
+			fmt.Println("skipping malformed row:", row)
+			continue
+		}
+
+		user := User{
+			Id:    row[0],
+			Email: row[1],
+			Name:  row[2],
+			Phone: row[3],
+		}
+
+		fmt.Printf("User: %+v\n", user)
 	}
 }
