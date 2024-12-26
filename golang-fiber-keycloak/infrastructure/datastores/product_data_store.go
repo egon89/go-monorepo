ffdb3f42d684b63bce1b335c8a7236831e2f6711
everton_goncalves <everton.goncalves.89@gmail.com>
2024-12-25 22:46:38 -0300

    feat: product entity
    - golang-fiber-keycloak

---
+++ golang-fiber-keycloak/infrastructure/datastores/product_data_store.go
@@ -0,0 +1,39 @@
+package datastores
+
+import (
+	"golang-fiber-keycloak/domain/entities"
+	"slices"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type productDataStore struct {
+	products map[uuid.UUID]entities.Product
+	sync.Mutex
+}
+
+func NewProductDataStore() *productDataStore {
+	return &productDataStore{
+		products: make(map[uuid.UUID]entities.Product),
+	}
+}
+
+func (ds *productDataStore) Create(product *entities.Product) {
+	ds.Lock()
+	ds.products[product.Id] = *product
+	ds.Unlock()
+}
+
+func (ds *productDataStore) GetAll() []entities.Product {
+	all := make([]entities.Product, 0, len(ds.products))
+	for _, value := range ds.products {
+		all = append(all, value)
+	}
+
+	slices.SortFunc(all, func(i, j entities.Product) int {
+		return i.CreatedAt.UTC().Compare(j.CreatedAt.UTC())
+	})
+
+	return all
+}
