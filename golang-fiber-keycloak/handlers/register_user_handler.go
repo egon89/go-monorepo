5acc5e47181710494901a6ff9a5d056760f7d8be
everton_goncalves <everton.goncalves.89@gmail.com>
2025-01-02 22:13:36 -0300

    feat: register user handler
    - golang-fiber-keycloak

---
+++ golang-fiber-keycloak/handlers/register_user_handler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"context"
+	registeruser "golang-fiber-keycloak/use_case/user/register-user"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/pkg/errors"
+)
+
+type RegisterUserUseCase interface {
+	Register(ctx context.Context, request registeruser.RegisterUserInput) (registeruser.RegisterUserOutput, error)
+}
+
+func RegisterUserHandler(useCase RegisterUserUseCase) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		ctx := c.UserContext()
+		var input = registeruser.RegisterUserInput{}
+
+		err := c.BodyParser(&input)
+		if err != nil {
+			return errors.Wrap(err, "unable to parse incoming request")
+		}
+
+		output, err := useCase.Register(ctx, input)
+		if err != nil {
+			return errors.Wrapf(err, "error creating user %v", input.Username)
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(output)
+	}
+}
