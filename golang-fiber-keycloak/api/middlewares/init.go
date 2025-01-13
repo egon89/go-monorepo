1dd72bc206c907d20cf4d2bd3e799f38a578819c
Everton Goncalves <everton.goncalves.89@gmail.com>
2025-01-12 22:48:33 -0300

    feat: add middleware for protected routes
    - golang-fiber-keycloak

--- golang-fiber-keycloak/api/middlewares/init.go
+++ golang-fiber-keycloak/api/middlewares/init.go
@@ -1,8 +1,14 @@
 package middlewares
 
 import (
+	"context"
+	"golang-fiber-keycloak/infrastructure/identity"
+	"golang-fiber-keycloak/shared/enums"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 )
 
 func InitFiberMiddlewares(
@@ -10,8 +16,26 @@ func InitFiberMiddlewares(
 	initPublicRoutes func(app *fiber.App),
 	initProtectedRoutes func(app *fiber.App),
 ) {
+	app.Use(requestid.New())
 	app.Use(logger.New())
 
+	app.Use(func(c *fiber.Ctx) error {
+		// get the request id that was added by requestid middleware
+		requestId := c.Locals("requestid")
+
+		// create a new context and add the requestId to it
+		ctx := context.WithValue(context.Background(), enums.ContextKeyRequestId, requestId)
+		c.SetUserContext(ctx)
+
+		return c.Next()
+	})
+
 	initPublicRoutes(app)
+
+	tokenRetrospector := identity.NewIdentityManager()
+	app.Use(NewJwtMiddleware(tokenRetrospector))
+
 	initProtectedRoutes(app)
+
+	log.Println("fiber middlewares initialized")
 }
