741cc1448adcb63b943ea7977baaecc784004078
Everton Goncalves <everton.goncalves.89@gmail.com>
2025-09-07 22:30:48 -0300

    feat: working with pointer
    - pointers and errors

--- learn-go-with-tests/pointers-and-errors/wallet.go
+++ learn-go-with-tests/pointers-and-errors/wallet.go
@@ -6,11 +6,14 @@ type Wallet struct {
 	balance int // private outside the package it's defined in
 }
 
-func (w Wallet) Deposit(amount int) {
+// so rather than taking a copy of the whole Wallet,
+// we instead take a pointer to that wallet so that we can change the original values within it.
+func (w *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %p and wallet %p\n", &w.balance, &w)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
-	return w.balance
+func (w *Wallet) Balance() int {
+	// return (*w).balance  // without explicit dereference
+	return w.balance // automatically dereferenced
 }
