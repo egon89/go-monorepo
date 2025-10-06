9df0d9a77d468835df755c473318f2855460f3e0
everton_goncalves <everton.goncalves.89@gmail.com>
2025-10-06 12:11:20 -0300

    feat: update map
    - map

--- learn-go-with-tests/maps/dictionary.go
+++ learn-go-with-tests/maps/dictionary.go
@@ -7,8 +7,9 @@ type DictionaryErr string
 // We made the errors constant; this required us to create our own DictionaryErr type which implements the error interface
 // it makes the errors more reusable and immutable
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
 )
 
 func (d Dictionary) Search(word string) (string, error) {
@@ -29,6 +30,21 @@ func (d Dictionary) Add(word, definition string) error {
 		d[word] = definition
 	case nil:
 		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
 	default:
 		return err
 	}
