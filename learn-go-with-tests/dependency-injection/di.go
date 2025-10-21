58d6baeb8e621cb101741c07da38023103c0d74e
Everton Goncalves <everton.goncalves.89@gmail.com>
2025-10-20 22:46:00 -0300

    feat: using interface for dep. injection
    - dependency-injection

--- learn-go-with-tests/dependency-injection/di.go
+++ learn-go-with-tests/dependency-injection/di.go
@@ -3,14 +3,26 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	// os.Stdout implements io.Writer
-	Greet(os.Stdout, "John Doe")
+	Greet(os.Stdout, "John Doe\n")
+
+	fmt.Println("starting server on 5001...")
+
+	log.Fatal(http.ListenAndServe(":5001",
+		http.HandlerFunc(MyGreeterHandler)))
 }
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	// http.ResponseWriter implements io.Writer
+	Greet(w, "Jane Doe")
 }
