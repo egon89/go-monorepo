01349d7cbc71c015bfd442768d6b7bc11e276747
Everton Goncalves <everton.goncalves.89@gmail.com>
2025-11-07 23:16:42 -0300

    feat: using spy in tests
    - mocking

---
+++ learn-go-with-tests/mocking/countdown.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func main() {
+	sleeper := &DefaultSleeper{}
+
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprint(out, i, "\n")
+
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(out, finalWord)
+}
---
+++ learn-go-with-tests/mocking/countdown_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+const write = "write"
+const sleep = "sleep"
+
+/*
+ * Spies are a kind of mock which can record how a dependency is used.
+ * They can record the arguments sent in, how many times it has been called, etc.
+ */
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func TestCountdown(t *testing.T) {
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
+
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
