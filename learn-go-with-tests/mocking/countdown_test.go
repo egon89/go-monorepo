f250f5d909dae3dd962d55ed08fd1607113d6e91
Everton Goncalves <everton.goncalves.89@gmail.com>
2025-11-08 01:50:25 -0300

    feat: configurable sleeper
    - mocking

--- learn-go-with-tests/mocking/countdown_test.go
+++ learn-go-with-tests/mocking/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 const write = "write"
@@ -24,6 +25,14 @@ func (s *SpyCountdownOperations) Sleep() {
 func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) SetDurationSlept(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func TestCountdown(t *testing.T) {
@@ -58,6 +67,18 @@ Go!`
 
 		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
 			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+
+	t.Run("configurable sleeper", func(t *testing.T) {
+		sleepTime := 5 * time.Second
+
+		spyTime := &SpyTime{}
+		sleeper := ConfigurableSleeper{sleepTime, spyTime.SetDurationSlept}
+		sleeper.Sleep()
+
+		if spyTime.durationSlept != sleepTime {
+			t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 		}
 	})
 }
